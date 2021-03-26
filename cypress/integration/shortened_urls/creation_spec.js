c2f86d15f9a77ab9d671bed84fa4435ee5c254d5
Maxime Morisset <maximemorisset@MacBook-Pro-de-Maxime.home>
2021-03-26 12:56:09 +0100

    Fix typos in tests

--- cypress/integration/shortened_urls/creation_spec.js
+++ cypress/integration/shortened_urls/creation_spec.js
@@ -1,9 +1,9 @@
-describe('Creation', () => {
+describe('shortened URL creation', () => {
   beforeEach(() => {
     cy.app('clean') // have a look at cypress/app_commands/clean.rb
   })
 
-  it('Successfully create a shortened URL when the URL is valid', () => {
+  it('successfully create a shortened URL when the URL is valid', () => {
     const URL = 'https://fr.wikipedia.org/wiki/Arctic_Monkeys';
     cy.visit('/');
     cy.get('.shortened_url_original_url').type(URL)
@@ -14,7 +14,7 @@ describe('Creation', () => {
     cy.get('.details').contains(URL);
   })
 
-  it('Show an error when no URL is given', () => {
+  it('shows an error when no URL is given', () => {
     cy.visit('/');
     cy.get('.action-shorten-url').click()
     cy.location().should((loc) => {
@@ -23,7 +23,7 @@ describe('Creation', () => {
     cy.get('.shortened_url_original_url .invalid-feedback').contains("Original url can't be blank");
   });
 
-  it('Show an error when the URL given is invalid', () => {
+  it('shows an error when the URL given is invalid', () => {
     cy.visit('/');
     cy.get('.shortened_url_original_url').type('not_an_url')
     cy.get('.action-shorten-url').click()
