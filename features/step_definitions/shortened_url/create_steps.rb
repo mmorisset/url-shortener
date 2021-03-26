f9eb16785006d6d6ac5f72ac193528728f65c8c8
Maxime Morisset <maximemorisset@MacBook-Pro-de-Maxime.home>
2021-03-26 12:56:53 +0100

    Remove cucumber features folders

--- features/step_definitions/shortened_url/create_steps.rb
+++
@@ -1,8 +0,0 @@
-Given('I fill the Original URL field with {string}') do |url|
-  fill_in 'shortened_url_original_url', with: url
-end
-
-Then('a new shorten url should be created') do
-  expect(ShortenedUrl.count).to eq(1)
-  @shortened_url = ShortenedUrl.first
-end
