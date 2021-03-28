829fc0aa753ce57ffbb428771773ff5b3c1f6c03
Maxime Morisset <maximemorisset@MacBook-Pro-de-Maxime.home>
2021-03-28 22:21:31 +0200

    Move to Postgres and add uuid

--- db/migrate/20210325215912_create_shortened_urls.rb
+++ db/migrate/20210325215912_create_shortened_urls.rb
@@ -1,8 +1,8 @@
 class CreateShortenedUrls < ActiveRecord::Migration[6.0]
   def change
-    create_table :shortened_urls do |t|
+    create_table :shortened_urls, id: :uuid do |t|
       t.string :original_url
-      t.string :short_url
+      t.string :token
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_204251) do
+ActiveRecord::Schema.define(version: 2021_03_25_215912) do
 
-  create_table "shortened_urls", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "shortened_urls", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "original_url"
-    t.string "short_url"
+    t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "token"
   end
 
 end
